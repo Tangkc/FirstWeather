ceec9c3cd7a1c4e17b5be45bc6f9910143d2517e
Tangkc <510472314@qq.com>
2017-06-27 16:40:07 +0800

    完成一级页面的编写

---
+++ app/src/main/java/com/example/tangkc/firstcode/fragment/ChooseAreaFragment.java
@@ -0,0 +1,10 @@
+package com.example.tangkc.firstcode.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Tangkc on 2017/6/27.
+ */
+
+public class ChooseAreaFragment extends Fragment {
+}
---
+++ app/src/main/java/com/example/tangkc/firstcode/util/HttpUtil.java
@@ -0,0 +1,8 @@
+package com.example.tangkc.firstcode.util;
+
+/**
+ * Created by Tangkc on 2017/6/27.
+ */
+
+public class HttpUtil {
+}
