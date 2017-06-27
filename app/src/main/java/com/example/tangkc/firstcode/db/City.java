0203c956d31dccbcb00f59fe59d0900e9adf6d48
Tangkc <510472314@qq.com>
2017-06-27 08:29:25 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/tangkc/firstcode/db/City.java
@@ -0,0 +1,37 @@
+package com.example.tangkc.firstcode.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Tangkc on 2017/6/27.
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
